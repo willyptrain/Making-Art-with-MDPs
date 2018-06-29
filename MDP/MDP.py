dd4cd95bc0458d8c55786b6a77b92866bf5ecde8
Will Peterson <willpeterson76@gmail.com>
2018-06-29 13:51:02 -0400

    update values func workin

--- MDP/MDP.py
+++ MDP/MDP.py
@@ -4,6 +4,8 @@ class MDP:
 	def __init__(self, height, width): #add parameter for exit_coordinates
 		self.values = np.zeros(shape=(height+2,width+2))
 		self.actions = []
+		self.height = height
+		self.width = width
 		self.action_abilities = ["up", "right", "left"]
 		self.actions.append(np.array([[0, 0.8, 0], [0.1, 0, 0.1], [0, 0, 0]]))
 		self.actions.append(np.rot90(self.actions[0]))
@@ -14,16 +16,18 @@ class MDP:
 
 	def update_values(self):
 		old_values = self.values
-		new_values = self.living_reward + self.discount*old_values
+		new_values = np.zeros(shape=(self.height+2, self.width+2))
+		new_values[1: len(new_values)-1, 1:len(new_values[0])-1] = self.living_reward + self.discount*old_values[1:len(self.values)-1, 1:len(self.values)-1]
+
 		for i in self.exit_coordinates["-1"]:
 			new_values[i[0]][i[1]] = "-1"
 		for i in self.exit_coordinates["1"]:
-			new_values[i[0]][i[1]] = "1"		
+			new_values[i[0]][i[1]] = "1"	
+			
 		self.values = new_values
 		
 	def convolve(self):
-		for action in self.actions:
-			print()
+		pass
 
 
 if __name__ == '__main__':
