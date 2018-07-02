dadce6e5c41cbcfd2ba02d03b14f3a1efad44c6b
Will Peterson <willpeterson76@gmail.com>
2018-07-02 16:35:49 -0400

    might be working

--- MDP/MDP.py
+++ MDP/MDP.py
@@ -12,13 +12,14 @@ class MDP:
 		self.actions.append(np.fliplr(self.actions[1]))
 		self.living_reward = -0.1
 		self.discount = 0.9
-		self.exit_coordinates = {"-1": [[0+1,2+1]], "1": [[1+1,2+1]]}
+		self.exit_coordinates = {"-1": [[1+1,2+1]], "1": [[0+1,2+1]]}
 		self.best_actions = []
+		self.max_rewards = np.zeros(shape=(height,width))
 
-	def update_values(self):
+	def update_values(self, t):
 		old_values = self.values
 		new_values = np.zeros(shape=(self.height+2, self.width+2))
-		new_values[1: len(new_values)-1, 1:len(new_values[0])-1] = self.living_reward + self.discount*old_values[1:len(self.values)-1, 1:len(self.values)-1]
+		new_values[1: len(new_values)-1, 1:len(new_values[0])-1] = old_values[1:len(self.values)-1, 1:len(self.values)-1] + (self.discount**t)*self.max_rewards
 		for i in self.exit_coordinates["-1"]:
 			new_values[i[0]][i[1]] = "-1"
 		for i in self.exit_coordinates["1"]:
@@ -28,6 +29,7 @@ class MDP:
 		
 	def convolve(self):
 		modified_values = np.zeros(shape=(self.height, self.width))
+		self.max_rewards = np.zeros(shape=(self.height, self.width))
 		self.best_actions = []
 		for r in range(1, len(self.values)-1):
 			for c in range(1, len(self.values[r])-1):
@@ -42,11 +44,12 @@ class MDP:
 					if(temp_value > largest):
 						largest = temp_value
 						largest_index = y
+				self.max_rewards[r-1][c-1] = temp_value
 				modified_values[r-1][c-1] = largest
-				self.best_actions.append(self.action_abilities[y])
-	
+				self.best_actions.append(self.action_abilities[largest_index])
 				
 		self.values = modified_values
+
 
 
 
@@ -55,14 +58,15 @@ if __name__ == '__main__':
 	mdp = MDP(3,3)
 	for i in range(0, 10):
 		#print(mdp.values)
-		mdp.update_values()
+		mdp.update_values(i)
 		#print(mdp.values)
 		mdp.convolve()
 		break
-	mdp.update_values()
-	print(mdp.values)
-	print(mdp.best_actions)
-
+	mdp.update_values(11)
+	print(mdp.values[1:len(mdp.values)-1,1:len(mdp.values[0])-1])
+	#print(mdp.best_actions)
+	for i in range(0, mdp.height):
+		print(mdp.best_actions[i:i+3])
 	'''
 		Example;
 			
